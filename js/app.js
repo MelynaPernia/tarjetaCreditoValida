1a6976e87b4f3ef35b29bc8db996228cd2dc0284
mebipeos <mebipeos@outlook.com>
2017-10-10 12:03:56 -0500

    Tarjeta de Credito Valida

--- js/app.js
+++ js/app.js
@@ -1,49 +1,59 @@
 
-//creando una funcion
+//creando una funcion llamada isValidCard con un parámetro numberCard
 var isValidCard= function(numberCard){
 
+  //Declarando la variable arrNumberReverse y se asigna los valores ingresados al revés
   var arrNumberReverse= ((numberCard.toString()).split('')).reverse();
+  //Creando un arreglo vacío
   var newArr=[];
+  //creando una variable sum inicializado en 0
   var sum=0;
+  //creando una variable sumDigit inicializado en 0
   var sumDigit=0;
 
-
+  //Creando un bucle For que recorre el arreglo arrNumberReverse
   for(var i=0;i<arrNumberReverse.length;i++) {
+      //A un nuevo arreglo llamado newArr se asigna los valores convertidos de string a enteros
       newArr[i]=parseInt(arrNumberReverse[i]);
-
   }
 
+  //Creando un bucle para recorrer el nuevo arreglo llamado newArr
   for(var j=0;j<newArr.length;j++){
+    //Si el residuo de la posición mas uno es cero
     if((j+1)%2===0){
+        //Si el elemento del arreglo newArr multiplicado por 2 es mayor igual que 10
         if(newArr[j]*2>=10){
+          //la sumDig es igual a la suma de los digitos
           sumDigit= Math.trunc((newArr[j]*2)/10)  + ((newArr[j]*2)%10);
+          //almaceno en el acumulador sum la suma de los digitos
           sum+=sumDigit;
         }else{
+          //almacena en sum el elemento del array multiplicado por 2
           sum+=newArr[j]*2;
         }
-
     }else{
-      sum+=newArr[j];
+        //almacena en sum el elemento del array
+        sum+=newArr[j];
     }
+  }
 
-  }
+  //Se crea una condición para validar si la sum mod 10 es 0
   if(sum%10===0){
-    alert( 'Numero de Tarjeta Valido');
+    //Muestra en una cajita el mensaje 'Número de tarjeta Válido'
+    alert( 'Número de tarjeta válido');
   }else{
-    alert('Numero de Tarjeta invalido');
+    //Muestra en una cajita el mensaje 'Número de tarjeta No Válido'
+    alert('Numero de tarjeta no válido');
   }
-//  alert(message);
+
 
 
 }
 do{
+  //creando la variable numberCard para ingresar el número de la tarjeta de crédito
   var numberCard=parseInt(prompt('Ingrese su tarjeta de credito'));
+  //Mientras el número de la tarjeta ingresada sea undefined seguir con el bucle
 }while (numberCard===undefined)
 
-
+//Llamando a la funcion isValidCard
 isValidCard(numberCard);
-/*
-do{
-//creando la variable numberCard para ingresar la tarjeta de credito
-var numberCard=parseInt(prompt('Ingrese su tarjeta de credito'));
-}while(numberCard===0)*/
